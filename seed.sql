d84843f7db89f17457e46209135895cef980869a
luiz.saraiva <luiz.saraiva@coollabs.com.br>
2024-08-14 19:23:11 -0300

    Update seed

--- seed.sql
+++ seed.sql
@@ -1,4 +1,3 @@
-
 USE [Livros]
 GO
 
@@ -78,3 +77,57 @@ GO
 ALTER TABLE [dbo].[Livro_Assunto]  WITH CHECK ADD FOREIGN KEY([Assunto_CodAs])
 REFERENCES [dbo].[Assunto] ([CodAs])
 GO
+
+CREATE TABLE [dbo].[Preco](
+	[CodPr] int IDENTITY(1,1) NOT NULL,
+	[Livro_CodL] int NOT NULL,
+	[Valor] numeric(10,2) NOT NULL,
+	[Tipo] int NOT NULL
+PRIMARY KEY CLUSTERED 
+(
+	[CodPr] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Preco]  WITH CHECK ADD FOREIGN KEY([Livro_CodL])
+REFERENCES [dbo].[Livro] ([CodL])
+GO
+
+CREATE VIEW [viewLivros] AS
+SELECT 
+	   au.Nome as Autor,
+       l.CodL as Codigo,
+	   l.Titulo,
+	   a.Descricao as Assunto,
+	   l.Editora,
+	   l.Edicao,
+	   l.AnoPublicacao as Ano	   
+FROM Autor au
+inner join Livro_Autor lau on au.CodAu = lau.Autor_CodAu
+inner join Livro l on lau.Livro_CodL = l.CodL
+Left join Livro_Assunto la on l.CodL =la.Livro_CodL
+Left join Assunto a on la.Assunto_CodAs = a.CodAs
+group by au.Nome, l.CodL,
+	   l.Titulo,
+	   a.Descricao,
+	   l.Editora,
+	   l.Edicao,
+	   l.AnoPublicacao;
+GO
+
+insert into Assunto values('Novas tecnologias');
+
+insert into Autor values('Luiz G. saraiva');
+
+insert into Livro values('Novas Tecnologias', 'Editora nova', 1, 2024);
+
+insert into Livro_Assunto values(1,1);
+
+insert into Livro_Autor values(1,1);
+
+insert into Preco values(1, 200,1);
+	   
+
+
+	   
